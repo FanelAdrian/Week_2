a5d22ad21f0decf4809355d5937722722be281e8
Fanel Adrian <faneladrianpop@gmail.com>
2022-10-25 13:23:35 +0200

    mobileProject

--- mobileProject/mobileTest.js
+++ mobileProject/mobileTest.js
@@ -2,6 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var mobile_1 = require("./mobile");
 var oneplus = new mobile_1.Mobile("one plus 7t pro Mclaren", "oneplus", "7t pro", "negro", 500);
-oneplus.setColor("negro anaranjado");
-oneplus.getName();
-oneplus.caracteristicas();
+var samsung = new mobile_1.Mobile("Galaxy Z Flip4", "samsung", "Z Flip", "morado", 1100);
+var oppo = new mobile_1.Mobile("Reno8 Pro 5g", "oppo", "Reno Pro ", "verde", 800);
+var huawei = new mobile_1.Mobile("HUAWEI Mate Xs 2", "huawei", "Mate Xs", "blanco", 2000);
+var myMobiles = [samsung, oppo, huawei];
+// oneplus.setColor("negro anaranjado");
+// oneplus.getName();
+// oneplus.caracteristicas();
+// samsung.caracteristicas();
+// oppo.caracteristicas();
+// huawei.caracteristicas();
+for (var _i = 0, myMobiles_1 = myMobiles; _i < myMobiles_1.length; _i++) {
+    var mobil = myMobiles_1[_i];
+    mobil.caracteristicas();
+}
--- mobileProject/mobileTest.ts
+++ mobileProject/mobileTest.ts
@@ -1,5 +1,14 @@
 import { Mobile } from "./mobile";
 let oneplus: Mobile = new Mobile("one plus 7t pro Mclaren","oneplus", "7t pro","negro",500);
-oneplus.setColor("negro anaranjado");
-oneplus.getName();
-oneplus.caracteristicas();
+let samsung: Mobile = new Mobile("Galaxy Z Flip4","samsung", "Z Flip","morado",1100);
+let oppo: Mobile = new Mobile("Reno8 Pro 5g","oppo", "Reno Pro ","verde",800);
+let huawei: Mobile = new Mobile("HUAWEI Mate Xs 2","huawei", "Mate Xs","blanco",2000);
+let myMobiles = [samsung, oppo, huawei];
+
+// oneplus.setColor("negro anaranjado");
+// oneplus.getName();
+// oneplus.caracteristicas();
+
+for (let mobil of myMobiles ){
+    mobil.caracteristicas()
+}
